fca2edfefd5c8c161169640358e8e7bf42f962c9
Deep Patel <61212533+deeppatel2899@users.noreply.github.com>
2021-05-17 15:31:39 -0700

    Create Cuboid.java

---
+++ Coding Exercise 36: Pool Area/Cuboid.java
@@ -0,0 +1,21 @@
+public class Cuboid extends Rectangle {
+    private double height;
+ 
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+ 
+        if(height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+ 
+    public double getHeight() {
+        return this.height;
+    }
+ 
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
