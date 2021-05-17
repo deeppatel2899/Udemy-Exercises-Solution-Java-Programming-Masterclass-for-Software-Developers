90acd881c652a95ca03825bff556dd08153fb783
Deep Patel <61212533+deeppatel2899@users.noreply.github.com>
2021-05-17 15:30:35 -0700

    Create Cylinder.java

---
+++ Coding Exercise 35: Cylinder/Cylinder.java
@@ -0,0 +1,18 @@
+public class Cylinder extends Circle {
+    private double height;
+    
+    public Cylinder(double radius, double height){
+        super(radius);
+        if(height<0){
+            this.height = 0;
+        }else{
+            this.height=height;
+        }
+    }
+    public double getHeight(){
+            return this.height;
+        }
+        public double getVolume(){
+            return (height*getArea());
+        }
+}
